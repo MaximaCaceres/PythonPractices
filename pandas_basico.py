164aa85865b3a25a66606f1e9d356412eb6c3848
MaximaCaceres <maximavanecaceres@gmail.com>
2025-07-29 10:41:17 -0300

    fix

--- pandas_basico.py
+++ pandas_basico.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np 
+import sys
 
 print("--- Cargando Datos del CSV ---")
 
@@ -10,7 +11,19 @@ try:
     print("Archivo CSV cargado exitosamente en un DataFrame.")
     print("Primeras 5 columnas del archivo:")
     print((df_sensores.head(5)))
-    
+
+    eleccion = int(input("\n¿Desea más información del archivo CSV? Pulse 1 para SÍ, o 0 para SALIR: "))
+    if eleccion == 1:
+            print("\n--- Mostrando más información del DataFrame... ---")
+            print("\nInformación general (df.info()):")
+            df_sensores.info() # Muestra un resumen conciso
+            print("\nEstadísticas descriptivas (df.describe()):")
+            print(df_sensores.describe()) # Muestra estadísticas básicas
+    elif eleccion == 0:
+            print("\nSaliendo del programa. ¡Hasta luego!")
+            
+            sys.exit() # sys.exit() termina la ejecución del scrip
+
 except FileNotFoundError:
 
     print("Error: El archivo 'datos_sensores_ejemplo.csv' no se encontró.")
